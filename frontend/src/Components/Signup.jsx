b67f14e404420d0074308776c3ddb2a04a1b8b4d
arishasehar <arshsehar66@gmail.com>
2025-04-28 00:48:15 +0500

    write

--- frontend/src/Components/Signup.jsx
+++ frontend/src/Components/Signup.jsx
@@ -46,10 +46,7 @@ const Signup = () => {
   return (
     <section className="relative py-6">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
-        <div className="">
-          <h1 className='text-[32px] font-extrabold boldFont w-40'>Trello</h1>
-        </div>
-
+       
         {loading ? (
           <div className="flex flex-col items-center justify-center my-4">
             <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-black"></div>
